8e56782ca3f7c835d5d9422291808a1df4563d8a
freecates <ramongil@ramongil.cat>
2018-10-04 17:18:21 +0200

    canviat el ga id

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga'
 
 export const initGA = () => {
   console.log('GA init')
-  ReactGA.initialize('UA-115360424-1')
+  ReactGA.initialize('UA-16449968-1')
 }
 
 export const logPageView = () => {
