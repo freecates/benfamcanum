9559aa5193b5e122bbd6491fed37cf79a56789bd
freecates <ramongil@ramongil.cat>
2018-11-07 16:43:55 +0100

    actualitzat nextjs i bootstrap

--- components/NavBarBenFamNum.js
+++ components/NavBarBenFamNum.js
@@ -20,40 +20,56 @@ export default class NavBarBenFamNum extends React.Component {
   render() {
     return (
       <div>
-        <Navbar style={{padding: '0 1rem 1rem 1rem'}} inverse toggleable expand='lg'>
-          <NavbarToggler title='Commutador' className='ml-2' onClick={this.toggle} />
-          <Link href="https://fanoc.org/"><a><NavbarBrand title='Ir a la Web Corporativa FANOC' className='ml-auto'>
-            <img src='/static/logo-families-nombroses.png' alt='Web Corporativa FANOC' />
-          </NavbarBrand></a></Link>
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem><Link prefetch href="/beneficios">
-                  <a title='Ofertas comerciales para familias numerosas'><strong>Ofertas y descuentos</strong></a>
-                </Link></NavItem>
-                <NavItem><Link prefetch href="/prestaciones">
-                  <a title='Prestaciones para familias numerosas'><strong>Ayudas públicas</strong></a>
-                </Link></NavItem>
-                <NavItem><Link prefetch href="/buscador">
-                  <a title='Buscador'><FontAwesome
-                name='search'
-                size='2x'
-                style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-              /></a>
-                </Link></NavItem>
-              </Nav>
-            </Collapse>
+        <Navbar style={{ padding: '0 1rem 1rem 1rem' }} inverse expand="lg">
+          <NavbarToggler title="Commutador" className="ml-2" onClick={this.toggle} />
+          <Link href="https://fanoc.org/">
+            <a>
+              <NavbarBrand title="Ir a la Web Corporativa FANOC" className="ml-auto">
+                <img src="/static/logo-families-nombroses.png" alt="Web Corporativa FANOC" />
+              </NavbarBrand>
+            </a>
+          </Link>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link prefetch href="/beneficios">
+                  <a title="Ofertas comerciales para familias numerosas">
+                    <strong>Ofertas y descuentos</strong>
+                  </a>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link prefetch href="/prestaciones">
+                  <a title="Prestaciones para familias numerosas">
+                    <strong>Ayudas públicas</strong>
+                  </a>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link prefetch href="/buscador">
+                  <a title="Buscador">
+                    <FontAwesome
+                      name="search"
+                      size="2x"
+                      style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                    />
+                  </a>
+                </Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </Navbar>
         <style jsx>{`
           a {
-            color:#000000;
-            margin:0 1em;
+            color: #000000;
+            margin: 0 1em;
           }
           a:hover {
-            color:#000000;
-            text-decoration:underline;
+            color: #000000;
+            text-decoration: underline;
           }
           a strong {
-            text-transform:uppercase;
+            text-transform: uppercase;
           }
           @media screen and (min-width: 768px) {
             .align-nav {
@@ -61,7 +77,7 @@ export default class NavBarBenFamNum extends React.Component {
           }
           @media screen and (min-width: 1024px) {
             .align-nav {
-              margin-top:5em;
+              margin-top: 5em;
             }
           }
         `}</style>
