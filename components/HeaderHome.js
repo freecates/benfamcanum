be71a6c2679f6d16f45f0b9bd64fe1d169cecdbf
freecates <ramongil@ramongil.cat>
2018-11-16 10:18:32 +0100

    performance improvements (render blocking resources)

--- components/HeaderHome.js
+++ components/HeaderHome.js
@@ -1,132 +1,149 @@
-import React from 'react'
-import Link from 'next/link'
-import NProgress from 'nprogress'
-import Router from 'next/router'
-import GlobalHead from './GlobalHead'
-import NavBarBenFamNum from './NavBarBenFamNum'
-import NavBarExternal from './NavBarExternal'
-import MediaQuery from 'react-responsive'
+import React from 'react';
+import Link from 'next/link';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import GlobalHead from './GlobalHead';
+import NavBarBenFamNum from './NavBarBenFamNum';
+import NavBarExternal from './NavBarExternal';
+import MediaQuery from 'react-responsive';
 
-Router.onRouteChangeStart = (url) => {
-  console.log(`Loading: ${url}`)
-  NProgress.start()
-}
-Router.onRouteChangeComplete = () => NProgress.done()
-Router.onRouteChangeError = () => NProgress.done()
+Router.onRouteChangeStart = url => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 const HeaderHome = () => (
   <React.Fragment>
-  <GlobalHead />
-  <MediaQuery minDeviceWidth={768}>
-  <NavBarExternal/>
-  </MediaQuery>
-  <div style={{ marginBottom: 20 }}>
-      <div className='fade-in'>
-        <div className='bg-white'>
+    <GlobalHead />
+    <MediaQuery minDeviceWidth={768}>
+      <NavBarExternal />
+    </MediaQuery>
+    <div style={{ marginBottom: 20 }}>
+      <div className="fade-in">
+        <div className="bg-white">
           <section>
             <NavBarBenFamNum />
           </section>
         </div>
         <div>
-          <img src='/static/bg-body-familias-numerosas.jpg' />
+          <picture>
+            <source type="image/webp" srcset="/static/bg-body-familias-numerosas.webp" />
+            <img src="/static/bg-body-familias-numerosas.jpg" alt="més és més" />
+          </picture>
         </div>
-          <section className='section-data'>
-            <div className='section-a'>
-              <h4 className='icones-prestacions align-center'>
-                  <Link prefetch href="/beneficios"><a title='Ofertas comerciales para familias numerosas'><img src='/static/icono-menu-fanoc-oferta.png'/><br/><strong>Ofertas y descuentos</strong></a></Link>
-              </h4>
-            </div>
-            <div className='section-b'>
-              <h4 className='icones-prestacions align-center'>
-                  <Link prefetch href="/prestaciones"><a title='Prestaciones para familias numerosas'><img src='/static/icono-menu-fanoc-prestaciones.png'/><br/><strong>Ayudas públicas</strong></a></Link>
-              </h4>
-            </div>
-          </section>
+        <section className="section-data">
+          <div className="section-a">
+            <h4 className="icones-prestacions align-center">
+              <Link prefetch href="/beneficios">
+                <a title="Ofertas comerciales para familias numerosas">
+                  <img src="/static/icono-menu-fanoc-oferta.png" />
+                  <br />
+                  <strong>Ofertas y descuentos</strong>
+                </a>
+              </Link>
+            </h4>
+          </div>
+          <div className="section-b">
+            <h4 className="icones-prestacions align-center">
+              <Link prefetch href="/prestaciones">
+                <a title="Prestaciones para familias numerosas">
+                  <img src="/static/icono-menu-fanoc-prestaciones.png" />
+                  <br />
+                  <strong>Ayudas públicas</strong>
+                </a>
+              </Link>
+            </h4>
+          </div>
+        </section>
       </div>
 
-      
-        <style jsx>{`
-          a {
-            color:#000000;
-            text-transform:uppercase;
-            transition: all 0.5s ease-in-out;
+      <style jsx>{`
+        a {
+          color: #000000;
+          text-transform: uppercase;
+          transition: all 0.5s ease-in-out;
+        }
+        a:hover {
+          color: #333333;
+          text-decoration: none;
+        }
+        .bg-white {
+          background: #ffffff;
+        }
+        ul,
+        section {
+          max-width: 70rem;
+          margin: 0 auto;
+          width: 100%;
+        }
+        ul.vertical.menu.align-center li {
+          text-align: center;
+        }
+        p {
+          color: #000000;
+        }
+        .align-center {
+          text-align: center;
+        }
+        .menu > li {
+          vertical-align: bottom;
+        }
+        .fade-in {
+          animation-name: fadeIn;
+          animation-duration: 1.3s;
+          animation-timing-function: cubic-bezier(0, 0, 0.4, 1);
+          animation-fill-mode: forwards;
+        }
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
           }
-          a:hover {
-            color:#333333;
-            text-decoration:none;
+          to {
+            opacity: 1;
           }
-          .bg-white {
-            background:#ffffff;
-          }
-          ul,section {
-            max-width:70rem;
-            margin:0 auto;
-            width:100%;
-          }
-          ul.vertical.menu.align-center li {
-            text-align:center;
-          }
-          p {
-            color:#000000;
-          }
-          .align-center {
-            text-align:center;
-          }
-          .menu>li {
-            vertical-align:bottom;
-          }
-          .fade-in {
-            animation-name: fadeIn;
-            animation-duration: 1.3s;
-            animation-timing-function: cubic-bezier(0, 0, 0.4, 1);
-            animation-fill-mode: forwards;
-          }
-          @keyframes fadeIn {
-            from {
-              opacity: 0;
-            }
-            to {
-              opacity: 1;
-            }
-          }
-          .section-a, .section-b {
-            padding: 1rem;
-            background:#cb5599;
-            margin:1em;
-          }
-          @media screen and (min-width: 320px) {
+        }
+        .section-a,
+        .section-b {
+          padding: 1rem;
+          background: #cb5599;
+          margin: 1em;
+        }
+        @media screen and (min-width: 320px) {
           header {
-              padding:0;
-            }
-            .icones-prestacions img {
-                padding:0 0 1em 0;
-            }
+            padding: 0;
           }
-          @media screen and (min-width: 768px) {
+          .icones-prestacions img {
+            padding: 0 0 1em 0;
+          }
+        }
+        @media screen and (min-width: 768px) {
           header {
-              padding:0;
-            }
-            .section-data {
-              display: -ms-flexbox;
-              display: flex;
-              -ms-flex-wrap: wrap;
-                  flex-wrap: wrap;
-              align-items:center;
-            }
-            .section-a, .section-b {
-              width: 45%;
-              max-width:42rem;
-            }
+            padding: 0;
           }
-          @media screen and (min-width: 1360px) {
-            .section-a, .section-b {
-              padding: 2rem 5rem;
-            }  
+          .section-data {
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+            align-items: center;
           }
-        `}</style>
-
+          .section-a,
+          .section-b {
+            width: 45%;
+            max-width: 42rem;
+          }
+        }
+        @media screen and (min-width: 1360px) {
+          .section-a,
+          .section-b {
+            padding: 2rem 5rem;
+          }
+        }
+      `}</style>
     </div>
-    </React.Fragment>
-)
+  </React.Fragment>
+);
 
-export default HeaderHome
+export default HeaderHome;
--- pages/ofertas-por-sectores.js
+++ pages/ofertas-por-sectores.js
@@ -1,116 +1,139 @@
-import Head from 'next/head'
-import Layout from '../components/MyLayout.js'
-import Link from 'next/link'
-import fetch from 'isomorphic-unfetch'
-import {IntlProvider, FormattedDate} from 'react-intl'
+import Head from 'next/head';
+import Layout from '../components/MyLayout.js';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+import { IntlProvider } from 'react-intl';
 
-const OfertasPorSectores = (props) => (
+const OfertasPorSectores = props => (
   <Layout layout>
     <Head>
       <title>Ofertas para familias numerosas por sectores</title>
     </Head>
     <nav aria-label="Estás aquí:" role="navigation">
-    <ul className="breadcrumbs">
-        <li><Link prefetch href="/"><a>Inicio</a></Link></li>
-        <li><Link prefetch href="/beneficios"><a>Ofertas para familias numerosas</a></Link></li>
+      <ul className="breadcrumbs">
         <li>
-        <span className="show-for-sr">Actual: </span> Sectores 
+          <Link prefetch href="/">
+            <a>Inicio</a>
+          </Link>
         </li>
-    </ul>
+        <li>
+          <Link prefetch href="/beneficios">
+            <a>Ofertas para familias numerosas</a>
+          </Link>
+        </li>
+        <li>
+          <span className="show-for-sr">Actual: </span> Sectores
+        </li>
+      </ul>
     </nav>
-    <IntlProvider defaultLocale='ca'>
+    <IntlProvider defaultLocale="ca">
       <main>
         <section>
-          <ul className='gallery'>
-          {props.ofertasporsectores.map((ofertasporsectore, index) => (           
-            <li className='item align-center' key={index}>
-              <Link prefetch as={`/c-ca/${ofertasporsectore.term_id}/${ofertasporsectore.slug}/Catalu/8143`} href={`/category-comunidad?sid=${ofertasporsectore.term_id}&comunidad=Catalu&caid=8143`}>
-                <a title={'Clica aquí para ver todas las ofertas de ' + ofertasporsectore.name}><img src={'/static/' + ofertasporsectore.slug +'-familias-numerosas.png'} width='96'/><br/><span dangerouslySetInnerHTML={ {__html: ofertasporsectore.name} } /></a>
-              </Link>
-            </li>
+          <ul className="gallery">
+            {props.ofertasporsectores.map((ofertasporsectore, index) => (
+              <li className="item align-center" key={index}>
+                <Link
+                  prefetch
+                  as={`/c-ca/${ofertasporsectore.term_id}/${ofertasporsectore.slug}/Catalu/8143`}
+                  href={`/category-comunidad?sid=${
+                    ofertasporsectore.term_id
+                  }&comunidad=Catalu&caid=8143`}
+                >
+                  <a title={'Clica aquí para ver todas las ofertas de ' + ofertasporsectore.name}>
+                    <img
+                      src={'/static/' + ofertasporsectore.slug + '-familias-numerosas.png'}
+                      width="96"
+                    />
+                    <br />
+                    <span dangerouslySetInnerHTML={{ __html: ofertasporsectore.name }} />
+                  </a>
+                </Link>
+              </li>
             ))}
-        </ul>
-
+          </ul>
         </section>
       </main>
     </IntlProvider>
-        <style jsx>{`
-          a, li {
-              color:#000000!important;
-          }
-          h1 {
-            color:#cb5599;
-            text-align:center;
-          }
-          .align-center {
-            text-align:center;
-          }
-          .gallery {
-            display: -ms-flexbox;
-            display: flex;
-            -ms-flex-wrap: wrap;
-                flex-wrap: wrap;
-            padding: 5px;
-          }
-          ul {
-            list-style-type:none;
-            margin-left:0;
-            margin:0 auto!important;
-          }
-          a {
-            color:inherit;
-          }
-          a:hover {
-            text-decoration:underline;
-          }
-          a.blue {
-            color:#00add9;
-            text-decoration:underline;
-          }
-          p {
-            margin-top:2rem;
-          }
-          .item {
-            width: 150px;
-          }
-          @media screen and (min-width: 320px) {   
-            .gallery {
-              width: 100%;
-            }              
-            .item {
-              margin: 5px;
-            }
-          }
-          @media screen and (max-width: 375px) {              
-            .item {
-              width: 124px;
-            }
-          }
-          @media screen and (min-width: 360px) {   
-            .gallery {
-              width: 90%;
-            }
-          }
-          @media screen and (min-width: 768px) {   
-            .gallery {
-              max-width: 70%;
-              width:500px;
-            }
-          .item {
-              width: 150px;
-            }
-          }
-        `}</style>
+    <style jsx>{`
+      a,
+      li {
+        color: #000000 !important;
+      }
+      h1 {
+        color: #cb5599;
+        text-align: center;
+      }
+      .align-center {
+        text-align: center;
+      }
+      .gallery {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-wrap: wrap;
+        flex-wrap: wrap;
+        padding: 5px;
+      }
+      ul {
+        list-style-type: none;
+        margin-left: 0;
+        margin: 0 auto !important;
+      }
+      a {
+        color: inherit;
+      }
+      a:hover {
+        text-decoration: underline;
+      }
+      a.blue {
+        color: #00add9;
+        text-decoration: underline;
+      }
+      p {
+        margin-top: 2rem;
+      }
+      .item {
+        width: 150px;
+      }
+      @media screen and (min-width: 320px) {
+        .gallery {
+          width: 100%;
+        }
+        .item {
+          margin: 5px;
+        }
+      }
+      @media screen and (max-width: 375px) {
+        .item {
+          width: 124px;
+        }
+      }
+      @media screen and (min-width: 360px) {
+        .gallery {
+          width: 90%;
+        }
+      }
+      @media screen and (min-width: 768px) {
+        .gallery {
+          max-width: 70%;
+          width: 500px;
+        }
+        .item {
+          width: 150px;
+        }
+      }
+    `}</style>
   </Layout>
-)
+);
 
 OfertasPorSectores.getInitialProps = async function() {
-  const res = await fetch('https://gestorbeneficios.familiasnumerosas.org/wp-json/lanauva/v1/categoria_del_beneficio')
-  const ofertasporsectores = await res.json()
+  const res = await fetch(
+    'https://gestorbeneficios.familiasnumerosas.org/wp-json/lanauva/v1/categoria_del_beneficio'
+  );
+  const ofertasporsectores = await res.json();
 
-  console.log(`Ofertas Por Sectores data fetched. Count: ${ofertasporsectores.length}`)
+  console.log(`Ofertas Por Sectores data fetched. Count: ${ofertasporsectores.length}`);
 
-  return { ofertasporsectores }
-}
+  return { ofertasporsectores };
+};
 
-export default OfertasPorSectores
+export default OfertasPorSectores;
