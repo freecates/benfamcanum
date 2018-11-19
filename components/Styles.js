a45118faa0ac659186306b5e19790c98c6b797b4
freecates <ramongil@ramongil.cat>
2018-11-19 11:17:23 +0100

    el·limanda la font Lato.

--- components/Styles.js
+++ components/Styles.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Styles = () => (
-    <section>
-        <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
-        <link rel="stylesheet" href="/static/bootstrap.min.css" />
-        <link rel="stylesheet" href="/static/foundation.min.css" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet" />
-        <link rel="stylesheet" href="/static/global.css" />
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous" />
-    </section>
-)
+  <section>
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <link rel="stylesheet" href="/static/bootstrap.min.css" />
+    <link rel="stylesheet" href="/static/foundation.min.css" />
+    <link rel="stylesheet" href="/static/global.css" />
+    <link
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossOrigin="anonymous"
+    />
+  </section>
+);
 
-export default Styles
+export default Styles;
