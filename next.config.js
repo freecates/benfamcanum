abce035c808ceba45d4330552529686dd921f2e3
Ramon Gil <ramon.gil@deporvillage.com>
2021-05-24 22:23:05 +0200

    with-10.2.3 , latest next and webpack

--- next.config.js
+++ next.config.js
@@ -25,14 +25,9 @@ const nextConfig = {
     ]
   },
   target: 'serverless',
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    };
-
-    return config;
-  }
+  future: {
+    webpack5: true,
+  },
 };
 
 module.exports = moduleExists('next-offline') ? withOffline(nextConfig) : nextConfig;
